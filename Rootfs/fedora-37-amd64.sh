c3739cf3badc402ca6df2e98d4a5c7c3a40b7bfa
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 15:06:44 +0700

    Update fedora-37-amd64.sh

--- Rootfs/fedora-37-amd64.sh
+++ Rootfs/fedora-37-amd64.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget "https://github.com/debuerreotype/docker-debian-artifacts/blob/905330be49c868aeb3985d5746873aa8d24c0e70/buster/rootfs.tar.xz?raw=true" -O debian-rootfs.tar.xz
-rm debian-buster-armhf.sh
+wget "https://github.com/fedora-cloud/docker-brew-fedora/blob/886b921b3dc7e58aea627372b0f5509a66d4dc38/x86_64/fedora-37-x86_64.tar.xz?raw=true" -O fedora-rootfs.tar.xz
+rm fedora-37-amd64.sh
