72b6a0e944832a00804c50212049905d3eeb4bd4
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 15:08:32 +0700

    Update fedora-37-arm64.sh

--- Rootfs/fedora-37-arm64.sh
+++ Rootfs/fedora-37-arm64.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 wget "https://github.com/fedora-cloud/docker-brew-fedora/blob/886b921b3dc7e58aea627372b0f5509a66d4dc38/aarch64/fedora-37-aarch64.tar.xz?raw=true" -O fedora-rootfs.tar.xz
 rm fedora-37-arm64.sh
-
