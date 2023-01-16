561b4bf09957ea23cb3ec4b1e522f1597bdf9aac
wahasa <69626847+wahasa@users.noreply.github.com>
2023-01-16 11:25:37 +0700

    Update de-xfce.sh

--- de-xfce.sh
+++ de-xfce.sh
@@ -9,6 +9,15 @@ dnf install tigervnc dbus -y
 #Get the extra components
 dnf install xfburn parole mousepad ristretto -y
 
+#Setup the necessary files
+mkdir -p ~/.vnc
+echo "#!/bin/bash
+export PULSE_SERVER=127.0.0.1
+xrdb $HOME/.Xresources
+startxfce4" > ~/.vnc/xstartup
+echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
+echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
+
 echo "#!/bin/sh
 export DISPLAY=:1
 export PULSE_SERVER=127.0.0.1
@@ -16,23 +25,23 @@ rm -rf /run/dbus/dbus.pid
 dbus-daemon --system
 dbus-launch xfce4-session" > /usr/local/bin/vncstart
 clear
+chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/vncstart
+chmod +x /usr/local/bin/vnc-start
+chmod +x /usr/local/bin/vnc-stop
 
 echo " "
 echo "Installing browser,.."
 echo " "
-apk add firefox
+dnf install firefox -y
 clear
 echo " "
 echo "Vnc Server address will run at 127.0.0.1:5901"
 echo " "
-echo "In Termux"
-echo "Start Vnc Server, run vncstart"
-echo "Stop  Vnc Server, run vncstop"
+echo "Start Vnc Server, run vnc-start"
 echo " "
-echo "In Linux"
-echo "Start Vnc, run vncstart"
-echo "Exit  Vnc, run ctrl+c"
+echo "Stop Vnc Server, run vnc-stop"
 echo " "
 
 rm de-xfce.sh
+vnc-start
