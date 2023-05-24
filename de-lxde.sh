545d76c59c9da070a06b47a66d2fa1079f298323
wahasa <69626847+wahasa@users.noreply.github.com>
2023-05-24 14:54:25 +0700

    Update de-lxde.sh

--- de-lxde.sh
+++ de-lxde.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #Get the necessary components
 dnf update
 dnf install sudo -y
@@ -20,26 +19,18 @@ echo "#!/bin/sh
 export DISPLAY=:1
 export PULSE_SERVER=127.0.0.1
 rm -rf /run/dbus/dbus.pid
-dbus-daemon --system
 dbus-launch lxsession" > /usr/local/bin/vncstart
-clear
 chmod +x ~/.vnc/xstartup
-chmod +x /usr/local/bin/vncstart
-chmod +x /usr/local/bin/vnc-start
-chmod +x /usr/local/bin/vnc-stop
-
-echo " "
-echo "Installing browser,.."
-echo " "
-dnf install firefox -y
-clear
-echo " "
-echo "Vnc Server address will run at 127.0.0.1:5901"
-echo " "
-echo "Start Vnc Server, run vnc-start"
-echo " "
-echo "Stop Vnc Server, run vnc-stop"
-echo " "
-
+chmod +x /usr/local/bin/*
+   clear
+   echo ""
+   echo "Installing browser,.."
+   echo ""
+   dnf install firefox -y
+   clear
+   echo ""
+   echo "Vnc Server address will run at 127.0.0.1:5901"
+   echo "Start Vnc Server, run vnc-start"
+   echo "Stop Vnc Server, run vnc-stop"
+   echo ""
 rm de-lxde.sh
-vnc-start
