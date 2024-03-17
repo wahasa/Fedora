1b38666dac0dc4e7cf95999f463ee56f4b52e696
wahasa <69626847+wahasa@users.noreply.github.com>
2024-03-17 09:41:17 +0700

    de-lxde.sh

--- Desktop/de-lxde.sh
+++ Desktop/de-lxde.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Get the necessary components
 dnf update
-dnf install sudo -y
+dnf install nano sudo -y
 dnf install @lxde-desktop-environment -y
 dnf install tigervnc-server dbus -y
 mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
@@ -30,6 +30,7 @@ chmod +x /usr/local/bin/*
    clear
    echo ""
    echo "Vnc Server address will run at 127.0.0.1:5901"
+   echo ""
    echo "Start Vnc Server, run vnc-start"
    echo "Stop  Vnc Server, run vnc-stop"
    echo ""
