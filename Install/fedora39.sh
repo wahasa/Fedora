7bd26090abfd7301f568108e7248b522155af951
wahasa <69626847+wahasa@users.noreply.github.com>
2024-10-04 12:59:08 +0700

    fedora39.sh

--- Install/fedora39.sh
+++ Install/fedora39.sh
@@ -2,13 +2,14 @@
 pkg install root-repo x11-repo
 pkg install proot xz-utils pulseaudio -y
 termux-setup-storage
-fedora=39
+fedora=40
+build=20240929
 folder=fedora-fs
 if [ -d "$folder" ]; then
         first=1
         echo "Skipping Downloading"
 fi
-tarball="fedora-rootfs.tar.xz"
+tarball="fedora-rootfs.tar"
 if [ "$first" != 1 ];then
         if [ ! -f $tarball ]; then
                 echo "Download Rootfs, this may take a while base on your internet speed."
@@ -24,7 +25,7 @@ if [ "$first" != 1 ];then
                 *)
                         echo "Unknown Architecture"; exit 1 ;;
                 esac
-                wget "https://github.com/fedora-cloud/docker-brew-fedora/raw/${fedora}/${archurl}/fedora-${fedora}-${archurl}.tar.xz" -O $tarball
+		wget "https://github.com/fedora-cloud/docker-brew-fedora/raw/${fedora}/${archurl}/fedora-${build}.tar" -O $tarball
         fi
         cur=`pwd`
         mkdir -p "$folder"
@@ -40,6 +41,7 @@ if [ "$first" != 1 ];then
 mkdir -p $folder/binds
 bin=.fedora
 linux=fedora
+echo ""
 echo "writing launch script"
 cat > $bin <<- EOM
 #!/bin/bash
@@ -82,34 +84,50 @@ else
     \$command -c "\$com"
 fi
 EOM
-   #echo "Fixing shebang of $linux"
-   termux-fix-shebang $bin
-   #echo "Making $linux executable"
-   chmod +x $bin
-   #echo "Fixing permissions for $linux"
-   chmod -R 755 $folder
-   #echo "Removing image for some space"
-   #rm $tarball
+    #echo "Fixing shebang of $linux"
+    termux-fix-shebang $bin
+    #echo "Making $linux executable"
+    chmod +x $bin
+    #echo "Fixing permissions for $linux"
+    chmod -R 755 $folder
+    #echo "Removing image for some space"
+    #rm $tarball
 echo "export PULSE_SERVER=127.0.0.1" >> $folder/etc/skel/.bashrc
 echo '#!/bin/bash
 bash .fedora' > $PREFIX/bin/$linux
 chmod +x $PREFIX/bin/$linux
-   clear
-   echo ""
-   echo "Add Fedora Package,.."
-   echo ""
+    clear
+    echo ""
+    echo "Add Fedora Package,.."
+    echo ""
 echo "#!/bin/bash
-dnf install ncurses nano sudo -y
+dnf install ncurses nano sudo dialog -y
 cp /etc/skel/.bashrc .
 rm -rf ~/.bash_profile
 exit" > $folder/root/.bash_profile
-bash $linux
-   clear
-   echo ""
-   echo "You can login to Fedora with 'fedora' script next time"
-   echo ""
-   #rm fedora39.sh
-
-#
-## Script edited by 'WaHaSa', Script V3-revision.
-#
+    bash $bin
+    echo 'PRETTY_NAME="Fedora 40 (Container Image)"
+NAME="Fedora"
+VERSION_ID=40
+VERSION="40 (Container Image)"
+ID=fedora
+ANSI_COLOR="0;38;2;60;110;180"
+HOME_URL="https://fedoraproject.org"
+DOCUMENTATION_URL="https://docs.fedoraproject.org/en-US"
+SUPPORT_URL="https://ask.fedoraproject.org"
+BUG_REPORT_URL="https://bugzilla.redhat.com"
+REDHAT_BUGZILLA_PRODUCT="Fedora"
+REDHAT_BUGZILLA_PRODUCT_VERSION=rawhide
+REDHAT_SUPPORT_PRODUCT="Fedora"
+REDHAT_SUPPORT_PRODUCT_VERSION=rawhide
+VARIANT="Container Image"
+VARIANT_ID=container
+LOGO=fedora-logo' > ~/"$folder"/etc/os-release
+    clear
+    echo ""
+    echo "You can login to Fedora with 'fedora' script next time"
+    echo ""
+    #rm fedora40.sh
+ #
+### Script edited by 'WaHaSa', Script revision-4.
+ #
