b8e730851d67bb7559c3dd0962a14beed5e64b79
wahasa <69626847+wahasa@users.noreply.github.com>
2024-11-19 12:46:25 +0700

    fedora40.sh

--- Install/fedora40.sh
+++ Install/fedora40.sh
@@ -1,13 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg install root-repo x11-repo
-pkg install proot xz-utils pulseaudio -y
-termux-setup-storage
+pkg install proot xz-utils neofetch pulseaudio -y
+#termux-setup-storage
 fedora=40
-build=20241013
+build=20241117
+    echo ""
+    neofetch --ascii_distro Fedora -L
 folder=fedora-fs
 if [ -d "$folder" ]; then
         first=1
-        echo "Skipping Downloading"
+        echo "Skipping Downloading."
 fi
 tarball="fedora-rootfs.tar"
 if [ "$first" != 1 ];then
@@ -17,38 +19,37 @@ if [ "$first" != 1 ];then
                 aarch64)
                         archurl="aarch64" ;;
                 #arm*)
-                #       archurl="armhf" ;;
-                ppc64le)
-                        archurl="ppc64le" ;;
+                        #archurl="armhfp" ;;
+                #i386)
+		        #archurl="x86" ;;
                 x86_64)
                         archurl="x86_64" ;;
                 *)
-                        echo "Unknown Architecture"; exit 1 ;;
+                        echo "Unknown Architecture."; exit 1 ;;
                 esac
-		wget "https://github.com/fedora-cloud/docker-brew-fedora/raw/${fedora}/${archurl}/fedora-${build}.tar" -O $tarball
+		wget -q --show-progress "https://github.com/fedora-cloud/docker-brew-fedora/raw/${fedora}/${archurl}/fedora-${build}.tar" -O $tarball
         fi
         cur=`pwd`
-        mkdir -p "$folder"
+        mkdir -p $folder
 	mkdir -p $folder/binds
         cd "$folder"
         echo "Decompressing Rootfs, please be patient."
-        proot --link2symlink tar -xf ${cur}/${tarball}||:
+        proot --link2symlink tar -xpf ${cur}/${tarball} --strip-components=1 --exclude json --exclude VERSION --exclude='dev'||:
+	#tar -xpf layer.tar
         cd "$cur"
    fi
-   echo "fedora" > ~/"$folder"/etc/hostname
-   echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
-   echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
-mkdir -p $folder/binds
+   echo "localhost" > $folder/etc/hostname
+   echo "127.0.0.1 localhost" > $folder/etc/hosts
+   echo "nameserver 8.8.8.8" > $folder/etc/resolv.conf
 bin=.fedora
 linux=fedora
 echo ""
-echo "writing launch script"
+echo "Writing launch script"
 cat > $bin <<- EOM
-#!/bin/bash
-pulseaudio --start \
-    --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
-    --exit-idle-time=-1
+#!/data/data/com.termux/files/usr/bin/bash
 cd \$(dirname \$0)
+## Start pulseaudio
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 ## Unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
 command="proot"
@@ -63,19 +64,31 @@ if [ -n "\$(ls -A $folder/binds)" ]; then
 fi
 command+=" -b /dev"
 command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
+command+=" -b /dev/null:/proc/stat"
+command+=" -b /dev/urandom:/dev/random"
 command+=" -b /proc"
+command+=" -b /proc/self/fd:/dev/fd"
+command+=" -b /proc/self/fd/0:/dev/stdin"
+command+=" -b /proc/self/fd/1:/dev/stdout"
+command+=" -b /proc/self/fd/2:/dev/stderr"
+command+=" -b /sys"
 command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
+command+=" -b $folder/tmp:/dev/shm"
 command+=" -b $folder/root:/dev/shm"
 ## Uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
 ## Uncomment the following line to mount /sdcard directly to /
 command+=" -b /sdcard"
+command+=" -b /mnt"
 command+=" -w /root"
 command+=" /usr/bin/env -i"
 command+=" HOME=/root"
+command+=" PATH=/bin:/usr/bin:/sbin:/usr/sbin"
 command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
 command+=" TERM=\$TERM"
 command+=" LANG=C.UTF-8"
+command+=" LC_ALL=C"
+command+=" LANGUAGE=en_US"
 command+=" /bin/bash --login"
 com="\$@"
 if [ -z "\$1" ];then
@@ -84,50 +97,37 @@ else
     \$command -c "\$com"
 fi
 EOM
-    #echo "Fixing shebang of $linux"
+    echo ""
+    echo "Fixing shebang of $linux"
     termux-fix-shebang $bin
-    #echo "Making $linux executable"
+    echo "Making $linux executable"
     chmod +x $bin
-    #echo "Fixing permissions for $linux"
+    echo "Fixing permissions for $linux"
     chmod -R 755 $folder
-    #echo "Removing image for some space"
+    echo "Removing image for some space"
+    #rm -rf $folder/layer.tar
     #rm $tarball
+echo ""
 echo "export PULSE_SERVER=127.0.0.1" >> $folder/etc/skel/.bashrc
-echo '#!/bin/bash
-bash .fedora' > $PREFIX/bin/$linux
+echo "TZ='Asia/Jakarta'; export TZ" > $folder/root/.profile
+echo 'bash .fedora' > $PREFIX/bin/$linux
 chmod +x $PREFIX/bin/$linux
     clear
     echo ""
-    echo "Add Fedora Package,.."
+    echo "Checking Packages,.."
     echo ""
 echo "#!/bin/bash
-dnf install ncurses nano sudo dialog -y
-cp /etc/skel/.bashrc .
+touch ~/.hushlogin
+dnf install dialog nano sudo ncurses tzdata -y
+cp .bashrc .bashrc.bak ; cp /etc/skel/.bashrc .
 rm -rf ~/.bash_profile
 exit" > $folder/root/.bash_profile
     bash $bin
-    echo 'PRETTY_NAME="Fedora 40 (Container Image)"
-NAME="Fedora"
-VERSION_ID=40
-VERSION="40 (Container Image)"
-ID=fedora
-ANSI_COLOR="0;38;2;60;110;180"
-HOME_URL="https://fedoraproject.org"
-DOCUMENTATION_URL="https://docs.fedoraproject.org/en-US"
-SUPPORT_URL="https://ask.fedoraproject.org"
-BUG_REPORT_URL="https://bugzilla.redhat.com"
-REDHAT_BUGZILLA_PRODUCT="Fedora"
-REDHAT_BUGZILLA_PRODUCT_VERSION=40
-REDHAT_SUPPORT_PRODUCT="Fedora"
-REDHAT_SUPPORT_PRODUCT_VERSION=40
-VARIANT="Container Image"
-VARIANT_ID=container
-LOGO=fedora-logo' > ~/"$folder"/etc/os-release
     clear
     echo ""
     echo "You can login to Fedora with 'fedora' script next time"
     echo ""
     #rm fedora40.sh
  #
-### Script edited by 'WaHaSa', Script revision-4.
- #
+### Script edited by 'WaHaSa', Script revision-5.
+ ##
