dff278b2da55cba0dcb49cf318099a60c348e0b5
wahasa <69626847+wahasa@users.noreply.github.com>
2025-01-05 18:04:27 +0700

    fedoradev.sh

--- Install/fedoradev.sh
+++ Install/fedoradev.sh
@@ -4,7 +4,7 @@ pkg install proot xz-utils neofetch pulseaudio -y
 #termux-setup-storage
 echo ""
 fedora=42
-build=20241229
+build=20250105
 neofetch --ascii_distro Fedora -L
 folder=fedora-fs
 if [ -d "$folder" ]; then
