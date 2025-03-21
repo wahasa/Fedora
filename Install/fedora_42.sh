f4176b75ff5246a76f0fc1336a9b9f260ed79052
wahasa <69626847+wahasa@users.noreply.github.com>
2025-03-21 13:59:02 +0700

    fedora_42.sh

--- Install/fedora_42.sh
+++ Install/fedora_42.sh
@@ -4,7 +4,7 @@ pkg install proot neofetch pulseaudio -y
 #termux-setup-storage
 echo ""
 fedora=42
-build=20250105
+build=20250119
 neofetch --ascii_distro Fedora -L
 folder=fedora-fs
 if [ -d "$folder" ]; then
@@ -49,8 +49,8 @@ pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth
 ## Unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
 command="proot"
-command+=" --kill-on-exit"
 command+=" --link2symlink"
+command+=" --kill-on-exit"
 command+=" -0"
 command+=" -r $folder"
 if [ -n "\$(ls -A $folder/binds)" ]; then
@@ -59,30 +59,31 @@ if [ -n "\$(ls -A $folder/binds)" ]; then
    done
 fi
 command+=" -b /dev"
-command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
-command+=" -b /dev/null:/proc/stat"
 command+=" -b /dev/urandom:/dev/random"
+command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
 command+=" -b /proc"
 command+=" -b /proc/self/fd:/dev/fd"
 command+=" -b /proc/self/fd/0:/dev/stdin"
 command+=" -b /proc/self/fd/1:/dev/stdout"
 command+=" -b /proc/self/fd/2:/dev/stderr"
-command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
-command+=" -b $folder/root:/dev/shm"
+command+=" -b /sys"
+command+=" -b /data/data/com.termux"
+command+=" -b $folder/tmp:/dev/shm"
 ## Uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
 ## Uncomment the following line to mount /sdcard directly to /
 command+=" -b /sdcard"
+command+=" -b /mnt"
 command+=" -w /root"
 command+=" /usr/bin/env -i"
 command+=" HOME=/root"
-command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 command+=" TERM=\$TERM"
 command+=" LC_ALL=C"
 command+=" LANG=en_US.UTF-8"
 command+=" LANGUAGE=en_US"
 command+=" /bin/bash --login"
-com="\$@"
+com=" \$@"
 if [ -z "\$1" ];then
    exec \$command
 else
@@ -119,7 +120,7 @@ bash $bin
      echo ""
      echo "You can login to Linux with 'fedora' script next time"
      echo ""
-     #rm fedoradev.sh
+     #rm fedora42.sh
 #
 ## Script edited by 'WaHaSa', Script revision-5.
 #
